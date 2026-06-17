c26df7bb656c8edf383775e1cfc9e58b2453b352
ucallithappyhour <u.call.it.happy.hour@gmail.com>
2026-06-17 13:59:10 -0400

    Switch to legacy Supabase anon ke

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://xrrowiwkhbfvvnsepgjk.supabase.co";
-const supabaseAnonKey = "sb_publishable_GFuhKC74LWarBEHj_b_D1w_mRoj4zYa";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhycm93aXdraGJmdnZuc2VwZ2prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMjMzOTAsImV4cCI6MjA5NjY5OTM5MH0.HsSIKm6OVuv5jTFDuPJ0luu4qBlcXMqCHEolATiiq3g";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
